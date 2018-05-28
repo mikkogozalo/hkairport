24dcdf89030c01dd7728d25e70cd77b97806177a
Mikko Gozalo <mikgozalo@gmail.com>
2018-05-29 01:43:00 +0800

    Update geobox

--- fr/management/commands/update_fr_data.py
+++ fr/management/commands/update_fr_data.py
@@ -35,7 +35,7 @@ class Command(BaseCommand):
             start = time.time()
             while True:
                 r = requests.get(
-                    'https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=27.87,3.72,95.01,130.43&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1',
+                    'https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=26.85,2.58,99.27,134.69&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1',
                     headers={'accept': 'application/json, text/javascript, */*; q=0.01',
                              'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
                              'referer': 'https://www.flightradar24.com/11.77,117.95/4'})
